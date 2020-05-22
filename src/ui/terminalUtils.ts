b379c7cdf0b6796732e3511ca02c17d6a139c360
Moritz Breit <mail@moritz-breit.de>
2020-05-22 11:24:18 +0200

    feat: support multiple Gemfiles for bundler commands

--- src/ui/terminalUtils.ts
+++ src/ui/terminalUtils.ts
@@ -1,12 +1,42 @@
 import * as vscode from 'vscode';
 
+import path = require('path');
+
+interface GemfileQuickPickItem extends vscode.QuickPickItem {
+  uri: vscode.Uri;
+}
+
 // Shared terminal for bundler commands
 let terminal: vscode.Terminal | undefined;
 
-function runInTerminal(command: string): void {
+function runInTerminal(command: string, cwd: string): void {
   terminal = terminal ?? vscode.window.createTerminal('bundler');
+  terminal.sendText(`cd ${cwd}`);
   terminal.sendText(command);
   terminal.show();
+}
+
+async function askForGemfileDirectory(): Promise<string | undefined> {
+  const gemfiles = await vscode.workspace.findFiles('**/Gemfile');
+
+  if (gemfiles.length === 0) {
+    vscode.window.showErrorMessage('No Gemfile found in current workspace');
+    return undefined;
+  }
+
+  if (gemfiles.length === 1) return path.dirname(gemfiles[0].fsPath);
+
+  const quickPickResult = await vscode.window.showQuickPick<GemfileQuickPickItem>(
+    gemfiles.map((uri) => {
+      const label = path.basename(path.dirname(uri.fsPath));
+      const detail = vscode.workspace.asRelativePath(uri);
+      return { label, detail, uri };
+    }),
+  );
+
+  if (quickPickResult === undefined) return undefined;
+
+  return path.dirname(quickPickResult.uri.fsPath);
 }
 
 export function registerTerminalCommand(
@@ -14,8 +44,9 @@ export function registerTerminalCommand(
   commandId: string,
   command: string,
 ): void {
-  const disposable = vscode.commands.registerCommand(commandId, () => {
-    runInTerminal(command);
+  const disposable = vscode.commands.registerCommand(commandId, async (cwdArg) => {
+    const cwd = cwdArg ?? await askForGemfileDirectory();
+    if (cwd !== undefined) runInTerminal(command, cwd);
   });
   context.subscriptions.push(disposable);
 }
